5c572f955f5598551929df3bedc7afd34fc203cf
Wesley <wspencer3295@yahoo.com>
2021-03-01 11:38:17 -0700

    commit tree 1f659a669dab4f6e3b7e3aa76b25bc04790e3fc8

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Cat{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+      return `${this.name} says meow!`
+    }
+}
+
+class Dog{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+      return `${this.name} says woof!`
+
+    }
+}
+
+class Bird{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        if(this.name === "Mable"){
+      return `${this.name} says squawk!`
+        }else{
+      return `It's me! ${this.name}, the parrot!`
+        }
+
+    }
+}
